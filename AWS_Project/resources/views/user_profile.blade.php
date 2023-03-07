8cce1ad77cec316b1951de80bdcfd0dd4095d5a7
GopiKrishnaK <bk492@student.aru.ac.uk>
2023-03-07 13:28:58 +0000

    adding changes to user profile page

--- AWS_Project/resources/views/user_profile.blade.php
+++ AWS_Project/resources/views/user_profile.blade.php
@@ -10,9 +10,22 @@
         </button>
         <img class = "company_logo" src={{asset('images/company_logo.jpeg')}} alt = 'company logo'>
    </div>
-   <form action="user_profile" method="POST" class="user_profile_form">
-    <input type="text" placeholder="first_name" value={{$user["first_name"]}}><br>
-    <input type="text" placeholder="last_name" value={{$user["last_name"]}}><br>
-    <input type="text" placeholder="email_address" value={{$user["email"]}}><br>
-   </form>
+   <div>
+    <table>
+        <th class="user_profile_table_header">
+            <img src = {{asset('images/profile_icon.jpg')}} alt="profile image" class="user_profile_icon">
+        <br>
+        <input type="button" value="Sign Out" class="user_signout_button">
+        </th>
+        <th>
+            <form action="user_profile" method="POST" class="user_profile_form">
+                <input type="text" placeholder="first_name" value={{$user["first_name"]}}><br>
+                <input type="text" placeholder="last_name" value={{$user["last_name"]}}><br>
+                <input type="text" placeholder="email_address" value={{$user["email"]}}><br>
+                <input type="button" value="update" class="update_button">
+            </form>
+        </th>
+    </table>
+   </div>
+   
 </body>
